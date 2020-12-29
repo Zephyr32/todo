6032f028e0e275076feea27b4f4bafe2b2692899
Bogdan <35693010+Zephyr32@users.noreply.github.com>
2020-12-29 02:21:52 +0200

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  items:string[];
+  newTask:string;
+  constructor(){
+    this.items=[];
+  }
   title = 'todo';
+ 
+
+  addTasks(){
+    if(this.newTask==""){
+
+    }else{
+      this.items.push(this.newTask);
+      this.newTask="";
+    }
+  }
+  removeTask(task){
+    this.items.splice(this.items.findIndex((val)=>{val=task.value}))
+  }
 }
